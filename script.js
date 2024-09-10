c21672666841922f32960445d2389dc14dd629aa
Brayan_Giraldo <giraldo8900@gmail.com>
2024-09-10 09:59:45 -0500

    yeah

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let numeros = [1,2,3,4,5,6]
-alert(numeros[4])
-alert(numeros[5])
-alert(numeros[1])
+let num = [1,5,3,6,2,8,9,10,20,4,5,6]
+if(num[0] == 1){
+    console.log('Numeros iguales')
+}
+
+switch(num[4]){
+    case 2:
+        console.log("El numero es el: " + num[2]);
+        break;
+    case 7: 
+       console.log("El numero es el: " + num[5]);
+       break;
+       default:     
+}
